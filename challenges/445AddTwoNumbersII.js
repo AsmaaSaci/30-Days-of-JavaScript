85f0566ff53a0a15544ddd01c7fa5ec676f548ec
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-03 09:46:46 +0100

    Update 445AddTwoNumbersII.js

--- challenges/445AddTwoNumbersII.js
+++ challenges/445AddTwoNumbersII.js
@@ -1,12 +1,25 @@
 var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0), current = dummy, carry = 0;
-    while (l1 || l2 || carry) {
-        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-        carry = Math.floor(sum / 10);
-        current.next = new ListNode(sum % 10);
-        current = current.next;
-        if (l1) l1 = l1.next;
-        if (l2) l2 = l2.next;
+    let stack1 = [], stack2 = [];
+    
+    while (l1) {
+        stack1.push(l1.val);
+        l1 = l1.next;
     }
-    return dummy.next;
+    while (l2) {
+        stack2.push(l2.val);
+        l2 = l2.next;
+    }
+    
+    let carry = 0, head = null;
+    while (stack1.length || stack2.length || carry) {
+        let sum = carry;
+        if (stack1.length) sum += stack1.pop();
+        if (stack2.length) sum += stack2.pop();
+        let node = new ListNode(sum % 10);
+        node.next = head;
+        head = node;
+        carry = Math.floor(sum / 10);
+    }
+    
+    return head;
 };
