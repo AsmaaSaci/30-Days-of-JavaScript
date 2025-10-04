fbdba8b99c20d661f5f66e1193f44f10c5993699
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-04 10:00:36 +0100

    Create 1232-CheckIfItIsAStraightLine.js

---
+++ challenges/1232-CheckIfItIsAStraightLine.js
@@ -0,0 +1,11 @@
+var checkStraightLine = function(coordinates) {
+    let [x0, y0] = coordinates[0];
+    let [x1, y1] = coordinates[1];
+    for (let i = 2; i < coordinates.length; i++) {
+        let [x, y] = coordinates[i];
+        if ((y1 - y0) * (x - x0) !== (y - y0) * (x1 - x0)) {
+            return false;
+        }
+    }
+    return true;
+};
