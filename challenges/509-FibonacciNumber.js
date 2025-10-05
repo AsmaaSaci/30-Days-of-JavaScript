dc93e83640c546273b2e1e94864b3ec5487b8da8
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-05 10:03:42 +0100

    Create 509-FibonacciNumber.js

---
+++ challenges/509-FibonacciNumber.js
@@ -0,0 +1,10 @@
+var fib = function(n) {
+    if (n < 2) return n;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+};
