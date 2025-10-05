c400381b5c7580d9bd3ab9f76c4c9fcbdee4c842
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-05 15:34:08 +0100

    Update 3512-MinimumOperationsToMakeArraySumDivisibleByK.js

--- challenges/3512-MinimumOperationsToMakeArraySumDivisibleByK.js
+++ challenges/3512-MinimumOperationsToMakeArraySumDivisibleByK.js
@@ -1,18 +1,5 @@
 var minOperations = function(nums, k) {
-  let heap = nums.sort((a, b) => a - b);
-  let ops = 0;
-  while (heap.length > 1 && heap[0] < k) {
-    let x = heap.shift();
-    let y = heap.shift();
-    let z = x * 2 + y;
-    insert(heap, z);
-    ops++;
-  }
-  return heap[0] < k ? -1 : ops;
+  if (k === 0) return 0;
+  const sum = nums.reduce((a, b) => a + b, 0);
+  return ((sum % k) + k) % k;
 };
-
-function insert(arr, val) {
-  let i = arr.findIndex(x => x > val);
-  if (i === -1) arr.push(val);
-  else arr.splice(i, 0, val);
-}
